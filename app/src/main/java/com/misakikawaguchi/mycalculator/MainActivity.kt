8fcee134663d43eeb081b7de0233b5649b82674f
川口美咲 <misaki@arcphilia.co.jp>
2020-12-30 17:32:25 +0900

    [Add]押したキーの値を確認する変数を追加

--- app/src/main/java/com/misakikawaguchi/mycalculator/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/mycalculator/MainActivity.kt
@@ -5,6 +5,13 @@ import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    // 最後に押されたキーが数値かどうか確認する
+    var lastNumeric: Boolean = false
+
+    // ドットを追加するかどうか確認する。trueの場合はドットを追加しないように制限する
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
