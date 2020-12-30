40bfdf282bb267f7c34b09820549fd1c192964fb
川口美咲 <misaki@arcphilia.co.jp>
2020-12-30 17:29:10 +0900

    [Add]onClick追加

--- app/src/main/java/com/misakikawaguchi/mycalculator/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/mycalculator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.misakikawaguchi.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // 電卓の数字をtvInputに表示するようにする
+    fun onDigit(view: View) {
+
     }
 }
