d2fad15a70f4a39e6573ea1cd15c0540a2d162ff
川口美咲 <misaki@arcphilia.co.jp>
2020-12-30 18:36:40 +0900

    [Add]計算の処理を追加

--- app/src/main/java/com/misakikawaguchi/mycalculator/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/mycalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,6 +49,93 @@ class MainActivity : AppCompatActivity() {
             lastNumeric = false
             // ドットを押せないようにする
             lastDot = false
+        }
+    }
+
+    // 計算する
+    fun onEqual(view: View) {
+        // 最後の入力が数値のみの場合
+        if (lastNumeric) {
+            //値を読み取る
+            var value = tvInput.text.toString()
+            var prefix = ""
+
+            // try ～ catch で、発生した例外を処理して、異常終了しないようにする
+            try {
+
+                // 値が「-」で始まる場合は、値を分離し、値を使用して計算を実行します。
+                if(value.startsWith("-")) {
+                    prefix = "-"
+                    value = value.substring(1)
+                }
+
+                // inputValueに「/」が含まれている場合
+                if (value.contains("/")) {
+
+                    // /を使用してinputValueを分割する
+                    val splitValue = value.split("/")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    // プレフィックスが空でない場合（-がある場合）は、最初の値、つまり1を追加します。
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    // 割り算する。「.0」を取り除いたものが計算結果として表示される
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+
+                    // inputValueに「*」が含まれている場合
+                }else if (value.contains("*")) {
+
+                    //*を使用してinputValueを分割する
+                    val splitValue = value.split("*")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    // プレフィックスが空でない場合（-がある場合）は、最初の値、つまり1を追加します。
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    // 掛け算する
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+
+                    // inputValueに「-」が含まれている場合
+                }else if (value.contains("-")) {
+
+                    // -を使用してinputValueを分割する
+                    val splitValue = value.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    // プレフィックスがからでない場合（-がある場合）は、最初の値、つまり1を追加する
+                    if(!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    // 引き算をする
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+
+                    // inputValueに「+」は含まれている場合
+                }else if(value.contains("+")) {
+
+                    // +を使用してinputValueを分割する
+                    val splitValue = value.split("+")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    // 足算をする
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+
+            } catch (e:ArithmeticException) {
+                e.printStackTrace()
+            }
         }
     }
 
